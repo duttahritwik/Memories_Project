4031915f97ff56ae4550fe2a25345d3c8f4cf62a
duttahritwik <duttahritwik@gmail.com>
2023-01-26 16:44:54 -0800

    database setup and initial styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+import memories from './images/memories.png'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 }
 
 export default App
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import Post from './Post/Post'
+
+const Posts = () => {
+  return (
+    <Fragment>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+    </Fragment>
+  )
+}
+
+export default Posts
